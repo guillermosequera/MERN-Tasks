71b0313be6724f22c8f40da3cc86dcb9c6588e3b
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-22 06:11:42 -0300

    creating the components and routing

---
+++ src/components/auth/NewAccount.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewAccount = () => {
+    return (
+        <h1>
+            From NewAccount
+        </h1>
+    );
+}
+
+export default NewAccount;
---
+++ src/components/projects/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <h1>
+            From Projects
+        </h1>
+    );
+}
+
+export default Projects;
