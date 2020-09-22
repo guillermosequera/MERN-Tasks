0cd3b5c974857f83813e8e458c85f05c622ad4f5
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-22 06:27:58 -0300

    creating the login form

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,10 +1,46 @@
 import React from 'react';
 
 const Login = () => {
+
+    const onChange = () => {
+
+    }
+
     return (
-        <h1>
-            From Login
-        </h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Login</h1>
+
+                <form>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Your Mail"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Your Password"
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <input type="submit" className="btn btn-primario btn-block"
+                        value="Login" />
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
 
