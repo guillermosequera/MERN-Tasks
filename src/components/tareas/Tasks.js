2284d6f5c8496fabfab3f37b782609bef6f909cc
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-23 06:47:18 -0300

    creating the task list

--- src/components/tareas/Tasks.js
+++ src/components/tareas/Tasks.js
@@ -2,26 +2,7 @@ import React from 'react';
 
 const Tasks = () => {
     return (
-        <div className="formulario">
-            <form>
-                <div className="contenedor-input">
-                    <input
-                        type="text"
-                        className="input-text"
-                        placeholder="Name Tasks"
-                        name="nombre"
-                    />
-                </div>
-
-                <div className="contenedor-input">
-                    <input
-                        type="submit"
-                        className="btn btn-primario btn-submit btn-block"
-                        value="Add Tasks"
-                    />
-                </div>
-            </form>
-        </div>
+        <p>Desde tarea</p>
     );
 }
 
