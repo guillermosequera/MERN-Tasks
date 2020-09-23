4c8f17536d5b34d73ca3ade4cdd1b845a7ca001d
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-23 05:23:57 -0300

    create component Sidebar

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <aside>
+            <h1>Mern<span>Tasks</span></h1>
+
+            <div className="proyectos">
+                <h2>Your Projects</h2>
+            </div>
+        </aside>
+    );
+}
+
+export default Sidebar;
