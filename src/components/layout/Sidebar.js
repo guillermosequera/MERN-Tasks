c7392f524b828314ec8e5a5ec38b2abfffcf60aa
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-23 05:32:40 -0300

    creating the form for new projects

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import NewProject from '../projects/NewProject';
+
 const Sidebar = () => {
     return (
         <aside>
             <h1>Mern<span>Tasks</span></h1>
+
+            <NewProject />
 
             <div className="proyectos">
                 <h2>Your Projects</h2>
---
+++ src/components/projects/NewProject.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+
+const NewProject = () => {
+    return (
+        <Fragment>
+        <button
+            type="button"
+            className="btn btn-block btn-primario"
+        >New Project</button>
+
+        <form
+            className="formulario-nuevo-proyecto"
+        >
+            <input
+                type="text"
+                className="input-text"
+                placeholder="Name Project"
+                name="nombre"
+            />
+
+            <input
+                type="submit"
+                className="btn btn-primario btn-block"
+                value="Add Project"
+            />
+        </form>
+        </Fragment>
+    );
+}
+
+export default NewProject;
