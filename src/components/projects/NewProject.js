c09107a4d52119e07dd94e68f4cd230610716e36
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-23 05:42:39 -0300

    state and functions for new projects

--- src/components/projects/NewProject.js
+++ src/components/projects/NewProject.js
@@ -1,6 +1,34 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const NewProject = () => {
+
+    //State para proyecto
+    const [proyecto, guardarProyecto] = useState({
+        nombre: ''
+    });
+
+    //extraer nombre de proyecto
+    const { nombre } = proyecto;
+
+    //lee los conenidos del input
+    const onChangeProyecto = e => {
+        guardarProyecto({
+            ...proyecto,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    //cuando el usuario envia un proyecto
+    const onSubmitProyecto = e => {
+        e.preventDefault();
+
+        //validando el proyecto
+
+        //agregar al state
+
+        //reiniciar el form
+    }
+
     return (
         <Fragment>
         <button
@@ -10,12 +38,15 @@ const NewProject = () => {
 
         <form
             className="formulario-nuevo-proyecto"
+            onSubmit={onSubmitProyecto}
         >
             <input
                 type="text"
                 className="input-text"
                 placeholder="Name Project"
                 name="nombre"
+                value={nombre}
+                onChange={onChangeProyecto}
             />
 
             <input
