a1807ff780148ede48dc76c9895a687e9209a486
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-29 04:51:00 -0300

    adding a project id and passing it to components

--- src/context/tasks/tareaState.js
+++ src/context/tasks/tareaState.js
@@ -4,7 +4,21 @@ import TareaReducer from './tareaReducer';
 
 const TareaState = props => {
     const initialState = {
-        tareas: [],
+        tareas: [
+            { nombre: 'Elegir Plataforma', estado: true, proyectoId: 1},
+            { nombre: 'Elegir Colores', estado: false, proyectoId: 2},
+            { nombre: 'Elegir Plataformas de pago', estado: false, proyectoId: 3},
+            { nombre: 'Elegir Hosting', estado: true, proyectoId: 4},
+            { nombre: 'Elegir Plataforma', estado: true, proyectoId: 1},
+            { nombre: 'Elegir Colores', estado: false, proyectoId: 2},
+            { nombre: 'Elegir Plataformas de pago', estado: false, proyectoId: 3},
+            { nombre: 'Elegir Plataforma', estado: true, proyectoId: 4},
+            { nombre: 'Elegir Colores', estado: false, proyectoId: 1},
+            { nombre: 'Elegir Plataformas de pago', estado: false, proyectoId: 2},
+            { nombre: 'Elegir Plataforma', estado: true, proyectoId: 3},
+            { nombre: 'Elegir Colores', estado: false, proyectoId: 4},
+            { nombre: 'Elegir Plataformas de pago', estado: false, proyectoId: 3},
+        ],
     }
 
 
@@ -12,7 +26,11 @@ const TareaState = props => {
     const [state, dispatch] = useReducer(TareaReducer, initialState);
 
     return (
-        <TareaContext.Provider>
+        <TareaContext.Provider
+            value={{
+                tareas: state.tareas
+            }}
+        >
             {props.children}
         </TareaContext.Provider>
     )
