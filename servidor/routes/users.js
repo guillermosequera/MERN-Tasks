621ad282b6ba8877573f5b195f5f6287a17a1e32
Guillermo Sequera <sequeracrespo@gmail.com>
2020-10-15 19:31:45 -0300

    adding the controller to create new users

--- servidor/routes/users.js
+++ servidor/routes/users.js
@@ -1,10 +1,12 @@
 // rutas para crear usuarios
 const express = require('express');
 const router = express.Router();
+const usuarioController = require('../controllers/usuarioController');
 
 // Crea un usuario
 // api/users
-router.post('/', () => {
-    
-});
+router.post('/', 
+    usuarioController.crearUsuario
+);
+
 module.exports = router;
