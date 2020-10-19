440c16d6b627eaaa40fa0d90f4b969b6e33ae31c
Guillermo Sequera <sequeracrespo@gmail.com>
2020-10-19 19:44:15 -0300

    inserting users in the DB

--- servidor/controllers/usuarioController.js
+++ servidor/controllers/usuarioController.js
@@ -1,6 +1,21 @@
+const Usuario = require('../models/Usuario');
 
 
+exports.crearUsuario = async (req, res) => {
+    
+    try {
+        let usuario;
 
-exports.crearUsuario = (req, res) => {
-    console.log(req.body);
+        //crea el nuevo usuario
+        usuario = new Usuario(req.body);
+
+        //guardar usuario
+        await usuario.save();
+
+        //Mensaje de confirmacion
+        res.send('Usuario creado correctamente')
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un error');
+    }
 }
