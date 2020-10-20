44354a324d20b9e0d3228a4f037edc4ebf2ffb7e
Guillermo Sequera <sequeracrespo@gmail.com>
2020-10-19 21:01:29 -0300

    creating a JWT

--- servidor/controllers/usuarioController.js
+++ servidor/controllers/usuarioController.js
@@ -1,6 +1,7 @@
 const Usuario = require('../models/Usuario');
 const bcryptjs = require('bcryptjs');
 const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 
 exports.crearUsuario = async (req, res) => {
@@ -32,8 +33,25 @@ exports.crearUsuario = async (req, res) => {
         //guardar usuario
         await usuario.save();
 
-        //Mensaje de confirmacion
-        res.json({ msg: 'Usuario creado correctamente'});
+        //crear y firmar el JWT
+        const payload = {
+            usuario: {
+                id: usuario.id
+            }
+        };
+
+        //firmar el JWT
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn: 3600
+        }, (error, token) => {
+            if(error) throw error;
+
+             //Mensaje de confirmacion
+            res.json({ token });
+
+        });
+
+       
     } catch (error) {
         console.log(error);
         res.status(400).send('Hubo un error');
